fc1d8e3934f17da88f28ce501cbda1353fae7083
Casey Miller <cd.miller1230@gmail.com>
2023-07-19 14:50:09 -0600

    can add file and open it

--- fridge/fridge.swift
+++ fridge/fridge.swift
@@ -11,25 +11,25 @@ import SwiftUI
 class StackWindow: NSObject, NSApplicationDelegate {
     
     private var statusItem: NSStatusItem!
+    private var menu: NSMenu!
+    private var urls: [URL] = []
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        menu = NSMenu()
+        
         if let button = statusItem.button {
             if #available(macOS 11.0, *) {
                 let icon = NSImage(systemSymbolName: "refrigerator.fill", accessibilityDescription: "fridge")
-                var config = NSImage.SymbolConfiguration(pointSize: 13, weight: .black)
+                let config = NSImage.SymbolConfiguration(pointSize: 13, weight: .black)
                 button.image = icon?.withSymbolConfiguration(config)
-            }
-            else {
-                // previous macOS versions not supported
+            } else { // previous macOS versions not supported
             }
         }
         setup()
     }
     
     func setup() {
-        let menu = NSMenu()
-        
         let addFileOption =  NSMenuItem(title: "Add File", action: #selector(addFileToStack), keyEquivalent: "a")
         menu.addItem(addFileOption)
         statusItem.menu = menu
@@ -41,22 +41,30 @@ class StackWindow: NSObject, NSApplicationDelegate {
         dialog.canChooseFiles = true
         dialog.canChooseDirectories = false
         dialog.allowsMultipleSelection = false
-        dialog.allowedContentTypes = [.pdf]
-        dialog.canDownloadUbiquitousContents = true
         dialog.canResolveUbiquitousConflicts = false
+        dialog.canDownloadUbiquitousContents = true
+        dialog.orderFrontRegardless()
+        dialog.allowedContentTypes = [.pdf]
         
         var fileChoice: URL?
         if dialog.runModal() == NSApplication.ModalResponse.OK {
             fileChoice = dialog.url
-        }
-        else {
-            // Cancel was pressed
+        } else { // Cancel was pressed
             return
         }
-        if fileChoice == nil {
-            // There has been a problem
+        
+        guard let url = fileChoice else {
+            // Something went wrong
             return
         }
-        //... use fileChoice
+                
+        let urlString = url.absoluteString
+        let filePath = NSMenuItem(title: urlString, action: #selector(openFile), keyEquivalent: "1")
+        menu.addItem(filePath)
+        urls.append(url)
+    }
+    
+    @objc func openFile() {
+        NSWorkspace.shared.open(urls[0])
     }
 }
