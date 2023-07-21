995e128f914e0733f0b175f066f2b5e4baa03306
Casey Miller <cd.miller1230@gmail.com>
2023-07-20 23:11:46 -0600

    added button highlighting

--- fridge/FridgeMenu.swift
+++ fridge/FridgeMenu.swift
@@ -10,33 +10,34 @@ import SwiftUI
 
 struct FridgeMenu: View {
     @StateObject private var fridgeModel = Fridge()
-    @State var isHover = false
+    @State var hovered = -1
     
     var body: some View {
-        HStack{
-            Text("FRIDGE").padding().font(.system(.title3).weight(.semibold)).foregroundColor(.white)
-            Spacer()
+        VStack(spacing: 0) {
             HStack{
-                Button{fridgeModel.openDialog()} label: {Image(systemName: "plus.circle")}.buttonStyle(.borderless)
-                
-                Button{ NSApplication.shared.terminate(nil) } label: {Image(systemName: "x.circle.fill")}.buttonStyle(.borderless)
-            }.padding()
-        }
-        
-        VStack {
+                Text("Fridge").padding().font(.system(.callout).weight(.semibold)).foregroundColor(.white)
+                Spacer()
+                HStack{
+                    Button{fridgeModel.openDialog()} label: {Image(systemName: "plus.circle")}.buttonStyle(.borderless)
+                    
+                    Button{ NSApplication.shared.terminate(nil) } label: {Image(systemName: "x.circle.fill")}.buttonStyle(.borderless)
+                }.padding()
+            }
+            Divider().padding().padding(.top, -25)
             ForEach(0..<fridgeModel.ffiles.count, id: \.self) { i in
                 if !fridgeModel.ffiles.isEmpty {
                     HStack {
-                        Button{fridgeModel.openFile(i)} label: {Text(fridgeModel.ffiles[i].filename)}.buttonStyle(.borderless)
+                        Button{fridgeModel.openFile(i)} label: {Text(fridgeModel.ffiles[i].filename).padding(8).frame(maxWidth: .infinity, alignment: .leading)}
+                            .buttonStyle(.borderless)
+                            .onHover{ hover in
+                                if hover { self.hovered = i}
+                                else { self.hovered = -1}}
+                            .background(self.hovered == i ? RoundedRectangle(cornerRadius: 3, style: .continuous).fill(Color.white.opacity(0.2)) : RoundedRectangle(cornerRadius: 3, style: .continuous).fill(Color.clear))
                         Spacer()
                         Button{fridgeModel.removeFile(i)} label: {Image(systemName: "minus.circle")}.buttonStyle(.borderless)
                     }
                 }
-            }
-        }.padding()
+            }.padding().padding(.top, -25)
+        }
     }
-}
-
-#Preview {
-    FridgeMenu()
 }
