7446aba4f74e2af510b9d41dc5248902a028c19e
Casey Miller <cd.miller1230@gmail.com>
2023-07-28 14:42:25 -0600

    added colorscheme, moved button definitions to separate file

--- fridge/FridgeMenu.swift
+++ fridge/FridgeMenu.swift
@@ -9,55 +9,59 @@ import Foundation
 import SwiftUI
 
 struct FridgeMenu: View, Themeable {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
     @StateObject private var fridgeModel = Fridge()
     @State private var menu = ["File", "File Group"]
     @State private var inEditMode = false
     @State private var isPopover = false
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
     
     var body: some View {
         VStack(spacing: 5) {
+            // MARK: Title Bar
             HStack{
                 Text("Fridge")
                     .padding([.leading, .trailing, .top])
                     .font(.system(.body))
-                    .foregroundColor(primaryColor)
+                    .foregroundColor(titleColor)
                 Spacer()
                 HStack{
+                    // MARK: Add File Button
                     if fridgeModel.ffiles.hasAvailableSlots {
-                        IconButton(systemName: "plus.circle", color: primaryColor, action: { inEditMode = false; self.isPopover.toggle() })
-                            .popover(isPresented: self.$isPopover, arrowEdge: .bottom) {
+                        IconButton(systemName: "plus.circle", color: titleColor, action: { inEditMode = false; self.isPopover.toggle() })
+                        .popover(isPresented: self.$isPopover, arrowEdge: .bottom) {
                                 PopoverMenu(
                                     MenuButton(text: "Add File", action: {fridgeModel.openDialog()}),
                                     MenuButton(text: "Add File Group", action: {})
                             )}
                     }
+                    // MARK: Edit/Remove File Button
                     if inEditMode {
-                        IconButton(systemName: "minus.circle.fill", color: primaryColor, isDynamic: false, action: {inEditMode.toggle()} )
+                        IconButton(systemName: "minus.circle", color: titleColor, isDynamic: false, action: {inEditMode.toggle()} )
                     } else {
-                        IconButton(systemName: "minus.circle", color: primaryColor, action: {inEditMode.toggle()} )
+                        IconButton(systemName: "minus.circle", color: titleColor, action: {inEditMode.toggle()} )
                     }
-                    IconButton(systemName: "x.circle", color: primaryColor, action: {inEditMode = false; NSApplication.shared.terminate(nil)} )
+                    // MARK: Quit Button
+                    IconButton(systemName: "x.circle", color: titleColor, action: {inEditMode = false; NSApplication.shared.terminate(nil)} )
                 }.padding([.leading, .trailing, .top])
             }
+            
             Divider().padding([.leading, .trailing])
             
+            // MARK: File Buttons
             if !fridgeModel.ffiles.isEmpty {
                 VStack(spacing: 5) {
                     ForEach(0..<fridgeModel.ffiles.count, id: \.self) { i in
                         ZStack {
                             if inEditMode || isPopover {
-                                FileButton(text: fridgeModel.ffiles[i].filename, isDynamic: false, action: {})
+                                FileButton(text: fridgeModel.ffiles[i].filename, isDynamic: false, action: {} )
                             } else {
                                 FileButton(text: fridgeModel.ffiles[i].filename, action: {fridgeModel.openFile(i)} )
                             }
                             HStack {
                                 if inEditMode {
                                     Spacer()
-                                    IconButton(systemName: "minus.circle",
-                                               color: Color.red,
-                                               isDynamic: false,
-                                               action: {fridgeModel.removeFile(i)} ).padding(.trailing)
+                                    IconButton(systemName: "minus.circle", color: Color.red, isDynamic: false, action: {fridgeModel.removeFile(i)} ).padding(.trailing)
                                 }
                             }
                         }.padding([.leading, .trailing], 8)
@@ -65,126 +69,6 @@ struct FridgeMenu: View, Themeable {
                 }
             }
             
-        }.padding(.bottom, 8)
-    }
-}
-
-// MARK: Buttons and Popover Menu
-
-struct IconButton: View, Themeable {
-    let action: () -> Void
-    let foreground: Color
-    let isDynamic: Bool
-    let systemName: String
-    @State var hovering = false
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
-    init(systemName: String, color foreground: Color, isDynamic: Bool = true, action: @escaping () -> Void) {
-        self.action = action
-        self.foreground = foreground
-        self.isDynamic = isDynamic
-        self.systemName = systemName
-    }
-    
-    var body: some View {
-        Button(action: action,
-               label: { Image(systemName: systemName)
-                .opacity((self.hovering && isDynamic) || (!isDynamic) ? 1.0 : 0.5)
-                .foregroundColor(foreground)
-        })
-        .buttonStyle(.borderless)
-        .onHover{ hover in hovering = hover }
-        .scaleEffect(self.hovering ? 1.1 : 1.0)
-    }
-}
-
-struct FileButton: View, Themeable {
-    let action: () -> Void
-    let isDynamic: Bool
-    let text: String
-    @State var hovering = false
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
-    init(text: String, isDynamic: Bool = true, action: @escaping () -> Void) {
-        self.action = action
-        self.isDynamic = isDynamic
-        self.text = text
-    }
-    
-    var body: some View {
-        Button(action: action, label: { Text(text)
-                .padding(.leading, 10)
-                .padding([.top, .bottom], 8)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(self.hovering && isDynamic ? alternateColor : primaryColor)
-        })
-        .buttonStyle(.borderless)
-        .onHover{ hover in hovering = hover }
-        .scaleEffect(self.hovering && isDynamic ? 1.015 : 1.0)
-        .background(self.hovering && isDynamic ?
-                    RoundedRectangle(cornerRadius: 5, style: .continuous).fill(highlightColor) :
-                    RoundedRectangle(cornerRadius: 5, style: .continuous).fill(Color.clear))
-    }
-}
-
-struct MenuButton: View, Themeable {
-    let action: () -> Void
-    let text: String
-    @State var hovering = false
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
-    init(text: String, action: @escaping () -> Void) {
-        self.action = action
-        self.text = text
-    }
-    
-    var body: some View {
-        Button(action: action, label: { Text(text)
-                .font(.callout)
-                .padding([.leading, .trailing], 5)
-                .padding([.top, .bottom], 3)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(self.hovering ? alternateColor : primaryColor)
-        })
-        .buttonStyle(.borderless)
-        .onHover{ hover in hovering = hover }
-        .scaleEffect(self.hovering ? 1.015 : 1.0)
-    }
-}
-
-struct PopoverMenu: View {
-    let firstOption: MenuButton
-    let secondOption: MenuButton
-    
-    init(_ addFile: MenuButton, _ addFileGroup: MenuButton) {
-        firstOption = addFile
-        secondOption = addFileGroup
-    }
-    
-    var body: some View {
-        VStack(alignment: .center, spacing: 5) {
-            firstOption
-            secondOption
-        }.padding(8)
-    }
-}
-
-// MARK: Behavior for light/dark mode
-
-protocol Themeable {
-    var colorScheme: ColorScheme { get }
-}
-
-extension Themeable {
-    var primaryColor: Color {
-        colorScheme == .dark ? .white : .black
-    }
-    
-    var alternateColor: Color {
-        colorScheme == .dark ? .black : .white
-    }
-    
-    var highlightColor: Color {
-        colorScheme == .light ? Color.black.opacity(0.2) : Color.white.opacity(0.2)
+        }.padding(.bottom, 8) // bottom of VStack
     }
 }
---
+++ fridge/UIElements.swift
@@ -0,0 +1,8 @@
+//
+//  UIElements.swift
+//  fridge
+//
+//  Created by Casey Miller on 2023-07-28.
+//
+
+import Foundation
