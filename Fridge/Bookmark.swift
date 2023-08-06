d79a34d3ccbb576c8f2ddddd9939a707397ffd77
Kai W. Li <stepperpig@gmail.com>
2023-08-06 14:47:36 -0500

    Added function doc comments

--- Fridge/Bookmark.swift
+++ Fridge/Bookmark.swift
@@ -11,24 +11,27 @@ import UniformTypeIdentifiers
 @available(macOS 13, *)
 public class Bookmark: Codable {
 
-    /// Bookmark-specific errors
+    /// Bookmark-specific errors.
     public enum BookmarkError: Error {
+        /// The file URL cannot be resolved.
         case invalidFileURL
+        /// Unable to access security scoped resource.
         case cantAccessSecurityScopedResource
+        /// The bookmark needs to be rebuilt (i.e., original targetURL is stale).
         case bookmarkIsStaleNeedsRebuild
     }
 
     /// The bookmark's state.
     public enum State {
-        /// The bookmark is valid (ie. the target url is resolvable)
+        /// The bookmark is valid (ie. the target url is resolvable).
         case valid
-        /// The bookmark needs to be rebuilt (eg. the original target file has been moved or renamed)
+        /// The bookmark needs to be rebuilt (eg. the original target file has been moved or renamed).
         case stale
-        /// The targetURL cannot be resolved, and as such the bookmark is invalid
+        /// The targetURL cannot be resolved, and as such the bookmark is invalid.
         case invalid
     }
 
-    /// The result of retrieving a URL from a bookmark
+    /// The URL retrieved from a bookmark.
     public struct TargetURL {
         public let state: Bookmark.State
         public let url: URL
@@ -38,9 +41,10 @@ public class Bookmark: Codable {
         }
     }
 
-    /// The raw bookmark data
+    /// The raw bookmark data.
     public let bookmarkData: Data
 
+    /// The state for any given bookmark. 
     public var state: State {
         guard let state = try? targetURL().state else {
             return .invalid
@@ -48,7 +52,7 @@ public class Bookmark: Codable {
         return state
     }
 
-    /// Create a bookmark from a URL
+    /// Create a bookmark from a URL. Checks if target file URL resolves to a valid file.
     public init(
         targetFileURL: URL,
         includingResourceValuesForKeys keys: Set<URLResourceKey>? = nil,
@@ -65,7 +69,7 @@ public class Bookmark: Codable {
         )
     }
     
-    /// Create a bookmark object from raw bookmark data
+    /// Create a bookmark object from raw bookmark data.
     public init(bookmarkData: Data, validate: Bool = true) throws {
         if validate {
             var isStale = false
@@ -78,7 +82,10 @@ public class Bookmark: Codable {
 // MARK: - Accessing the bookmark's target
 
 public extension Bookmark {
-    
+    /// Sets and returns the bookmark's target, including its state and its resolved URL.
+    ///
+    /// - Parameter options: The options considered when resolving security-scoped bookmark data.
+    /// - Returns: The TargetURL struct associated with the bookmark's target.
     @inlinable func targetURL(
         options: NSURL.BookmarkResolutionOptions = [.withSecurityScope]
     ) throws -> TargetURL {
@@ -87,6 +94,12 @@ public extension Bookmark {
         return TargetURL(result: isStale ? .stale : .valid, url: url)
     }
     
+    /// Accesses a bookmark's target URL and returns its state and URL.
+    ///
+    /// - Parameters:
+    ///   - options: The options considered when resolving security-scoped bookmark data. 
+    ///   - scopedBlock: Dummy variable used to cast URL type to specified return type.  
+    /// - Returns: The tuple containing the accessed TargetURL's state and URL. 
     func usingTargetURL<ReturnType>(
         options: NSURL.BookmarkResolutionOptions = [.withSecurityScope],
         _ scopedBlock: (URL) -> ReturnType
